aec9c0003cfe01475d6298898b20dbfdd6271b7d
Lixun Mao <mlx@lixun.wv.cc.cmu.edu>
2014-10-09 15:58:03 -0400

    up

--- src/main/Server.java
+++ src/main/Server.java
@@ -11,13 +11,14 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-
 public class Server {
 	public static Registry reg;
+
 	public static void main(String args[]) throws Exception {
-		int registryPort=0;
-		if (args.length != 2) {
-			System.out.format("Usage: ./Server <registryPort> <service_list>");
+		int registryPort = 0;
+		if (args.length > 2) {
+			System.out
+					.format("Usage1: ./Server <registryPort> <service_list>\nUsage2: ./Server <registryPort> <service_list>");
 			return;
 		}
 		try {
@@ -26,29 +27,33 @@ public class Server {
 			System.out.println("please input Integer as the port");
 			return;
 		}
-		String serviceList = args[1];
-		ArrayList<String> services=new ArrayList<String>(); 
-		try {
-			File file = new File(serviceList);
-			BufferedInputStream fis = new BufferedInputStream(
-					new FileInputStream(file));
-			BufferedReader b = new BufferedReader(new InputStreamReader(fis,
-					"utf-8"));
-			String line;
-			while ((line = b.readLine()) != null) {
-				services.add(line);
+		ArrayList<String> services = new ArrayList<String>();
+		if (args.length == 2) {
+			String serviceList = args[1];
+
+			try {
+				File file = new File(serviceList);
+				BufferedInputStream fis = new BufferedInputStream(
+						new FileInputStream(file));
+				BufferedReader b = new BufferedReader(new InputStreamReader(
+						fis, "utf-8"));
+				String line;
+				while ((line = b.readLine()) != null) {
+					services.add(line);
+				}
+				b.close();
+			} catch (Exception e) {
+				System.out.println("file list not found");
+				return;
 			}
-			b.close();
-		} catch (Exception e) {
-			System.out.println("file list not found");
-			return;
 		}
 		reg = new Registry(registryPort);
-		if(reg==null){
+		if (reg == null) {
 			System.out.println("bind registry port error");
 			System.exit(-1);
 		}
-		reg.addServices(services);
+		if (args.length == 2)
+			reg.addServices(services);
 		reg.listen();
 		System.out.println("Server close");
 	}
